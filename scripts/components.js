b8d34804ff72a06d9826a9710ad4f5c42fb9ad98
Lightling <contactlightling@gmail.com>
2021-05-18 14:23:07 -0400

    outline main/footer components

--- scripts/components.js
+++ scripts/components.js
@@ -4,6 +4,19 @@ const TEMPLATE_APP_HEADER = `
 </section>
 `;
 
+const TEMPLATE_APP_MAIN = `
+<section class="main">
+	<h2>{{ title }}</h2>
+</section>
+`;
+
+const TEMPLATE_APP_FOOTER = `
+<section class="footer">
+	<p>&copy; Lightling 2014-2021. All Rights Reserved.</p>
+	<p><a href="#">Back To Top</a></p>
+</section>
+`;
+
 const COMPONENT_APP_HEADER = {
 	template: TEMPLATE_APP_HEADER,
 	props: {
@@ -11,6 +24,22 @@ const COMPONENT_APP_HEADER = {
 	},
 };
 
+const COMPONENT_APP_MAIN = {
+	template: TEMPLATE_APP_MAIN,
+	props: {
+		title: String,
+	},
+};
+
+const COMPONENT_APP_FOOTER = {
+	template: TEMPLATE_APP_FOOTER,
+	props: {
+		
+	},
+};
+
 export {
 	COMPONENT_APP_HEADER,
+	COMPONENT_APP_MAIN,
+	COMPONENT_APP_FOOTER,
 }
