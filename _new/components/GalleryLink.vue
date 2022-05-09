39301f44b07607c246eeb97be56b0ebaaa0fd692
Lightling <contactlightling@gmail.com>
2022-05-09 16:21:35 -0400

    Create GalleryLink.vue

---
+++ _new/components/GalleryLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    :id="title"
+    :style="{ backgroundImage: imageURL }"
+    @click="$emit('click', title)"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    imageURL: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
