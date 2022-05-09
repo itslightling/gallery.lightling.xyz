73d12c74ed1c1a1bb3fc8d401d45e729eed20a71
Lightling <contactlightling@gmail.com>
2022-05-09 16:13:55 -0400

    Create Header.vue

---
+++ _new/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="header">
+    <h1>
+      {{ title }}
+    </h1>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'Header',
+})
+</script>
