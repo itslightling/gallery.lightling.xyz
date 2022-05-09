ffed981b1513313a6d9a74457b2df0f0b6389243
Lightling <contactlightling@gmail.com>
2022-05-09 16:13:57 -0400

    Create Footer.vue

---
+++ _new/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="footer">
+    <p>&copy; Lightling 2014-2022. All Rights Reserved.</p>
+    <p><a href="#">Back To Top</a></p>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'Footer',
+})
+</script>
