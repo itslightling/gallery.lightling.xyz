e41435e076083c6601100673f5a6e4b7c5d43e59
Lightling <contactlightling@gmail.com>
2022-05-09 16:22:45 -0400

    Create GalleryLinkWarning.vue

---
+++ _new/components/GalleryLinkWarning.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="warning">
+    <h4>Warning!</h4>
+    <p>
+      This gallery contains NSFW mature/adult material. Only browse if you are 18+ of age.
+    </p>
+    <div class="buttons">
+      <button
+        v-focus
+        @click="$emit('disable')"
+      >
+        Go Back
+      </button>
+      <button
+        @click="$emit('acknowledge')"
+      >
+        Acknowledge and Continue
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+
+  },
+})
+</script>
