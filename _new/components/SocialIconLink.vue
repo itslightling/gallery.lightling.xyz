cf34703c6acf74cf3bf13e0b10a04f4649a800ee
Lightling <contactlightling@gmail.com>
2022-05-09 16:18:46 -0400

    Create SocialIconLink.vue

---
+++ _new/components/SocialIconLink.vue
@@ -0,0 +1,41 @@
+<template>
+  <a
+    :href="href"
+  >
+    <img
+      :src="img"
+      :alt="alt"
+      :title="title"
+    />
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'SocialIconLink',
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    }
+  },
+  setup(props: any) {
+    const title = ref(props.title);
+    const alt: String = `${title} icon`;
+
+    return {
+      alt,
+    };
+  },
+})
+</script>
