f59c0767b137cd2613aaab2ed526257c176d899c
Lightling <contactlightling@gmail.com>
2022-06-17 00:12:51 -0400

    containerize gallery section/navigation

---
+++ components/Gallery/GalleryNavigationContainer.vue
@@ -0,0 +1,36 @@
+<template>
+  <section
+    class="gallery-nav"
+  >
+    <h2>Navigation</h2>
+    <ul>
+      <GalleryNavigation
+        v-for="(c, i) in galleryData"
+        :key="`nav_${i}`"
+        :content-prop="c"
+      />
+    </ul>
+    <!--<content-indicators></content-indicators>-->
+  </section>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'GalleryNavigationContainer',
+  props: {
+    galleryData: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup () {
+
+  },
+})
+</script>
---
+++ components/Gallery/GallerySectionContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <section
+    class="gallery"
+  >
+    <h2>Gallery</h2>
+    <GallerySection
+      v-for="(c, i) in galleryData"
+      :key="`section_${i}`"
+      :content-prop="c"
+      :level="0"
+    />
+  </section>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'GallerySectionContainer',
+  props: {
+    galleryData: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup () {
+
+  },
+})
+</script>
